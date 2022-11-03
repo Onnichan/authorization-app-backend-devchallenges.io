9b1e0e5b59d619761fd8414ccf5604b1142dcdfd
Onnichan <dyangeltk17@gmail.com>
2022-11-03 16:48:48 -0500

    :sparkles: Add new environment variables

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,10 +1,9 @@
 const { Sequelize } = require("sequelize");
-const { DATABASE, USER, PASSWORD,HOST } = require(".");
+const { DATABASE, USER, PASSWORD, HOST } = require(".");
 
 const sequelize = new Sequelize(DATABASE, USER, PASSWORD, {
   dialect: "mysql",
   host: HOST,
-
 });
 
 module.exports = sequelize;
